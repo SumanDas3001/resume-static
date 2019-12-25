4fc77990502766a0eaaa7ccee09314f28985908f
SumanDas3001 <sumandas3001@gmail.com>
2019-12-25 13:31:14 +0530

    Create header component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <div>
+      <header id="showcase">
+        <h1>Hey, I am Suman</h1>
+        <p>I am a javascript developer.Enjoy learning new things and love to work/contribute on open source development.</p>
+        <a href="#section-a" class="button" id="read-more">Read more</a>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
